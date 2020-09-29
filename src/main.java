19ed64c3fbe9bac0e909d3f78d8af42da67a5652
user <user@EF03>
2020-09-29 09:40:24 +0800

    change name

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm ³¯·s¤¯");
+		System.out.println("I'm newman");
 		System.out.println("My nid is d0641665");
 		System.out.println("This is my lab1 hw");
 		
